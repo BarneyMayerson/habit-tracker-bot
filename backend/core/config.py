5940d3579cc76feb63efbeaa3e3d2bdbd44490e3
Barney Mayerson <mr.barney.mayerson@gmail.com>
2025-11-09 10:23:42 +0300

    Setting up migrations with alembic

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,12 +1,26 @@
+import os
+
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     """FastAPI app settings."""
 
-    app_host: str = "127.0.0.1"
-    app_port: str = "8000"
+    db_conn: str = os.getenv("DB_CONNECTION", "postgresql+asyncpg")
+    db_user: str = os.getenv("DB_USERNAME", "user")
+    db_password: str = os.getenv("DB_PASSWORD", "password")
+    db_host: str = os.getenv("DB_HOST", "localhost")
+    db_port: str = os.getenv("DB_PORT", "5432")
+    db_name: str = os.getenv("DB_DATABASE", "database")
+
     debug: bool = False
+
+    @property
+    def database_url_async(self) -> str:
+        return f"{self.db_conn}://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf8", extra="ignore")
 
