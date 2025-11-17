d6bee9e90ae66f898d046ca585313faaa073a220
Barney Mayerson <mr.barney.mayerson@gmail.com>
2025-11-17 15:32:50 +0300

    Updating Settings class to the pydantic 2 style

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,32 +1,32 @@
-import os
-
-from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-load_dotenv()
 
 
 class Settings(BaseSettings):
-    """FastAPI app settings."""
+    """
+    FastAPI app settings using pydantic-settings.
+    All values are loaded from environment variables or .env file.
+    """
 
-    db_conn: str = os.getenv("DB_CONNECTION", "postgresql+asyncpg")
-    db_user: str = os.getenv("DB_USERNAME", "user")
-    db_password: str = os.getenv("DB_PASSWORD", "password")
-    db_host: str = os.getenv("DB_HOST", "localhost")
-    db_port: str = os.getenv("DB_PORT", "5432")
-    db_name: str = os.getenv("DB_DATABASE", "database")
+    db_connection: str = "postgresql+asyncpg"
+    db_username: str = "user"
+    db_password: str
+    db_host: str = "localhost"
+    db_port: str = "5432"
+    db_name: str = "database"
 
     debug: bool = False
 
     secret_key: str
 
-    habit_duration: int = os.getenv("HABIT_DURATION", 21)
+    habit_duration: int = 21
 
-    telegram_bot_tiken: str = os.getenv("HABIT_DURATION")
+    telegram_bot_token: str
 
     @property
     def database_url(self) -> str:
-        return f"{self.db_conn}://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+        return (
+            f"{self.db_connection}://{self.db_username}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+        )
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf8", extra="ignore")
 
